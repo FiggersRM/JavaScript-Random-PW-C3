9e4bc336eb893e848286441a6634cf73e49af2c3
FiggersRM <MitchFiggers@gmail.com>
2023-03-08 13:34:38 -0500

    Added a prompt to add uppercase letters to the password. Created a function to check and see if it was true, and added uppcase letters to the password if so. also got started on the lowercase letters.

--- script.js
+++ script.js
@@ -15,12 +15,39 @@ function generatePassword() {
   function checkLength () {
     while(passwordLength < 8 || passwordLength > 128) {
       passwordLength = prompt("Sorry, but it seems you have entered a number that does not match the criteria, please try again using a number between 8 and 128.");
+      return;
     }
   }
   checkLength ();
   var password = [];
   password.length = passwordLength;
   console.log(password.length);
+  var upperCase = confirm("Would you like to use uppercase letters in your password?");
+  console.log(upperCase);
+  function addUppercase () {
+    if (upperCase === true){
+      var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+      for (i = 0; i < password.length; i++) {
+        var randomChar = Math.floor(Math.random() * (uppercaseLetters.length + 1));
+        var randomIndex = Math.floor(Math.random() * password.length);
+        password[randomIndex] = uppercaseLetters[randomChar];
+      }
+    }
+  }
+  addUppercase();
+  console.log(password);
+  var lowerCase = confirm("Would you like to use lowercase letters in your password?");
+  console.log(lowerCase);
+  function addLowercase () {
+    if (lowerCase === true){
+      var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+      for (i = 0; i < password.length; i++) {
+        var randomChar = Math.floor(Math.random() * (uppercaseLetters.length + 1));
+        var randomIndex = Math.floor(Math.random() * password.length);
+        password[randomIndex] = uppercaseLetters[randomChar];
+      }
+    }
+  }
 }
 
 // Add event listener to generate button
