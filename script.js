7f9087479afa4ce29266dfe198dce7c0103e9055
FiggersRM <MitchFiggers@gmail.com>
2023-03-08 12:50:12 -0500

    Added the prompt for deciding the password length. Also added a while loop to ensure the length is between 8 and 128 characters.

--- script.js
+++ script.js
@@ -7,7 +7,20 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
+function generatePassword() {
+  var passwordLength = prompt("How many characters would you like your password to be? Must be between 8 and 128 characters.");
+  console.log(passwordLength);
+  function checkLength () {
+    while(passwordLength < 8 || passwordLength > 128) {
+      passwordLength = prompt("Sorry, but it seems you have entered a number that does not match the criteria, please try again using a number between 8 and 128.");
+    }
+  }
+  checkLength ();
+  var password = [];
+  password.length = passwordLength;
+  console.log(password.length);
 }
 
 // Add event listener to generate button
