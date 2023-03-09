3cffafe695f8a40babb41e6e1a9d3c250fc09163
FiggersRM <MitchFiggers@gmail.com>
2023-03-09 18:02:04 -0500

    Finished the Javascript code. Realized I was doing it inefficiently beforehand, leaving some values of the password array undefined. Fixed by creating a for loop for each position in the password array and then if else statements to check which character types were true and false, and properly concatinating the arrays based off of the results. This allowed every position in the password to have a value. Then returned the value as a string.

--- script.js
+++ script.js
@@ -10,73 +10,167 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var passwordLength = prompt("How many characters would you like your password to be? Must be between 8 and 128 characters.");
+  var passwordLength = prompt(
+    "How many characters would you like your password to be? Must be between 8 and 128 characters."
+  );
   console.log(passwordLength);
-  function checkLength () {
-    while(passwordLength < 8 || passwordLength > 128) {
-      passwordLength = prompt("Sorry, but it seems you have entered a number that does not match the criteria, please try again using a number between 8 and 128.");
+  function checkLength() {
+    while (passwordLength < 8 || passwordLength > 128) {
+      passwordLength = prompt(
+        "Sorry, but it seems you have entered a number that does not match the criteria, please try again using a number between 8 and 128."
+      );
     }
   }
-  checkLength ();
+  checkLength();
   var password = [];
   password.length = passwordLength;
   console.log(password.length);
-  var upperCase = confirm("Would you like to use uppercase letters in your password?");
+  var upperCase = confirm(
+    "Would you like to use uppercase letters in your password?"
+  );
   console.log(upperCase);
-  function addUppercase () {
-    if (upperCase === true){
-      var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-      for (i = 0; i < password.length; i++) {
-        var randomChar = Math.floor(Math.random() * (uppercaseLetters.length + 1));
-        var randomIndex = Math.floor(Math.random() * password.length);
-        password[randomIndex] = uppercaseLetters[randomChar];
-      }
-    }
-  }
-  addUppercase();
-  console.log(password);
-  var lowerCase = confirm("Would you like to use lowercase letters in your password?");
+  var lowerCase = confirm(
+    "Would you like to use lowercase letters in your password?"
+  );
   console.log(lowerCase);
-  function addLowercase () {
-    if (lowerCase === true){
-      var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-      for (i = 0; i < password.length; i++) {
-        var randomChar = Math.floor(Math.random() * (lowercaseLetters.length + 1));
-        var randomIndex = Math.floor(Math.random() * password.length);
-        password[randomIndex] = lowercaseLetters[randomChar];
-      }
-    }
-  }
-  addLowercase ();
-  console.log(password);
   var numbers = confirm("Would you like to add numbers to your password?");
   console.log(numbers);
-  function addNumbers () {
-    if (numbers === true){
-      var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-      for (i = 0; i < password.length; i++) {
-        var randomChar = Math.floor(Math.random() * (nums.length + 1));
-        var randomIndex = Math.floor(Math.random() * password.length);
-        password[randomIndex] = nums[randomChar];
-      }
-    }
-  }
-  addNumbers();
-  console.log(password);
-  var specChar = confirm("Would you like to add special characters to your password? Special characters include: !, ?, (, ), {, }, %, $, &, @, and *.");
+  var specChar = confirm(
+    "Would you like to add special characters to your password? Special characters include: !, ?, (, ), {, }, %, $, &, @, and *."
+  );
   console.log(specChar);
-  function addSpecChars () {
-    if (specChar === true){
-      var characters = ["!", "?", "(", ")", "{", "}", "%", "$", "&", "@", "*"];
-      for (i = 0; i < password.length; i++) {
-        var randomChar = Math.floor(Math.random() * (characters.length + 1));
-        var randomIndex = Math.floor(Math.random() * password.length);
-        password[randomIndex] = characters[randomChar];
+  var uppercaseLetters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  var lowercaseLetters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var characters = ["!", "?", "(", ")", "{", "}", "%", "$", "&", "@", "*"];
+  function createPassword() {
+    for (i = 0; i < password.length; i++) {
+      if (upperCase && lowerCase && numbers && specChar) {
+        var newArray = uppercaseLetters.concat(
+          lowercaseLetters,
+          nums,
+          characters
+        );
+        var randomChar = Math.floor(Math.random() * newArray.length);
+        password[i] = newArray[randomChar];
+      } else if (upperCase && lowerCase && numbers) {
+        var newArray = uppercaseLetters.concat(lowercaseLetters, nums);
+        var randomChar = Math.floor(Math.random() * newArray.length);
+        password[i] = newArray[randomChar];
+      } else if (upperCase && lowerCase && specChar) {
+        var newArray = uppercaseLetters.concat(lowercaseLetters, characters);
+        var randomChar = Math.floor(Math.random() * newArray.length);
+        password[i] = newArray[randomChar];
+      } else if (upperCase && numbers && specChar) {
+        var newArray = uppercaseLetters.concat(nums, characters);
+        var randomChar = Math.floor(Math.random() * newArray.length);
+        password[i] = newArray[randomChar];
+      } else if (lowerCase && numbers && specChar) {
+        var newArray = lowercaseLetters.concat(nums, characters);
+        var randomChar = Math.floor(Math.random() * newArray.length);
+        password[i] = newArray[randomChar];
+      } else if (upperCase && lowerCase) {
+        var newArray = uppercaseLetters.concat(lowercaseLetters);
+        var randomChar = Math.floor(Math.random() * newArray.length);
+        password[i] = newArray[randomChar];
+      } else if (upperCase && numbers) {
+        var newArray = uppercaseLetters.concat(nums);
+        var randomChar = Math.floor(Math.random() * newArray.length);
+        password[i] = newArray[randomChar];
+      } else if (upperCase && specChar) {
+        var newArray = uppercaseLetters.concat(characters);
+        var randomChar = Math.floor(Math.random() * newArray.length);
+        password[i] = newArray[randomChar];
+      } else if (lowerCase && numbers) {
+        var newArray = lowercaseLetters.concat(nums);
+        var randomChar = Math.floor(Math.random() * newArray.length);
+        password[i] = newArray[randomChar];
+      } else if (lowerCase && specChar) {
+        var newArray = lowercaseLetters.concat(characters);
+        var randomChar = Math.floor(Math.random() * newArray.length);
+        password[i] = newArray[randomChar];
+      } else if (numbers && specChar) {
+        var newArray = nums.concat(characters);
+        var randomChar = Math.floor(Math.random() * newArray.length);
+        password[i] = newArray[randomChar];
+      } else if (upperCase) {
+        var newArray = uppercaseLetters;
+        var randomChar = Math.floor(Math.random() * newArray.length);
+        password[i] = newArray[randomChar];
+      } else if (lowerCase) {
+        var newArray = lowercaseLetters;
+        var randomChar = Math.floor(Math.random() * newArray.length);
+        password[i] = newArray[randomChar];
+      } else if (numbers) {
+        var newArray = nums;
+        var randomChar = Math.floor(Math.random() * newArray.length);
+        password[i] = newArray[randomChar];
+      } else if (specChar) {
+        var newArray = characters;
+        var randomChar = Math.floor(Math.random() * newArray.length);
+        password[i] = newArray[randomChar];
       }
     }
   }
-  addSpecChars();
+  createPassword();
   console.log(password);
+  var stringPassword = password.toString();
+  console.log(stringPassword);
+  return stringPassword.replaceAll(",", "");
 }
 
 // Add event listener to generate button
